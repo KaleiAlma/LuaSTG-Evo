7adeaaff2b36a5ed4d5be182a53b5991b5ac10a2
Kalei Alma <kalei@tuta.io>
2024-10-06 17:37:30 -0700

    cleanup

--- LuaSTG/Core/Graphics/Font_OpenGL.cpp
+++ LuaSTG/Core/Graphics/Font_OpenGL.cpp
@@ -773,7 +773,6 @@ namespace Core::Graphics
                 {
                     start_pos.x = start.x;
                     start_pos.y -= line_height; // 向下换行
-                    spdlog::debug("[core] linebreak");
                     continue;
                 }
                 
