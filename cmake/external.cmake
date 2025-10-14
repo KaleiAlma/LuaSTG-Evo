214cca4f82f490acff027a9d26b851dffbfa78e4
Kalei Alma <kalei@tuta.io>
2025-10-14 04:36:52 -0700

    let's debug this

--- cmake/external.cmake
+++ cmake/external.cmake
@@ -8,9 +8,14 @@ string(TOUPPER ${LSTGEXT_LIB} LSTGEXT_LIB)
 if(NOT LSTGEXT_RELEASE)
     file(DOWNLOAD https://api.github.com/repos/${LSTGEXT_REPO}/releases/latest ${CMAKE_CURRENT_LIST_DIR}/latest.tmp)
     file(READ ${CMAKE_CURRENT_LIST_DIR}/latest.tmp LSTGEXT_LATEST_RESPONSE)
+    message("${LSTGEXT_LATEST_RESPONSE}")
+
     string(JSON LSTGEXT_RELEASE GET "${LSTGEXT_LATEST_RESPONSE}" "name")
     file(REMOVE ${CMAKE_CURRENT_LIST_DIR}/latest.tmp)
+
+    message("Using release: ${LSTGEXT_RELEASE}")
 endif()
+
 
 set(LSTGEXT_SYSTEM ${CMAKE_SYSTEM_NAME})
 
