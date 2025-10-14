1616db2af463a0df011c5fd02bdd453bfe2fcafc
Kalei Alma <kalei@tuta.io>
2025-10-14 05:57:14 -0700

    ok so windows might not have that quirk anymore?

--- LuaSTG/LuaSTG/LuaBinding/lua_random.cpp
+++ LuaSTG/LuaSTG/LuaBinding/lua_random.cpp
@@ -23,7 +23,7 @@ std::istream& operator>>(std::istream& src, __uint128_t& value) {
 	}
 	return src;
 }
-#ifndef _WIN32
+// #ifndef _WIN32
 static std::string u128_tostring(__uint128_t u128) {
 	std::string ret;
 	if (u128 > UINT64_MAX) {
@@ -35,7 +35,7 @@ static std::string u128_tostring(__uint128_t u128) {
 		return std::to_string(u64);
 	}
 }
-#endif
+// #endif
 
 static std::string_view const LibraryID("random");
 
@@ -603,15 +603,15 @@ namespace UtilRandom {
 		std::string serialize() {
 			std::ostringstream ss;
 			ss << name()
-#ifdef _WIN32
-				<< "-" << multiplier()
-				<< "-" << increment()
-				<< "-" << state_;
-#else
+// #ifdef _WIN32
+// 				<< "-" << multiplier()
+// 				<< "-" << increment()
+// 				<< "-" << state_;
+// #else
 				<< "-" << u128_tostring(multiplier())
 				<< "-" << u128_tostring(increment())
 				<< "-" << u128_tostring(state_);
-#endif
+// #endif
 			return ss.str();
 		}
 		bool deserialize(std::string const& data) {
