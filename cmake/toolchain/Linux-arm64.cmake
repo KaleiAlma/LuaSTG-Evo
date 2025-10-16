7dcae716de8755cd68d61c6f6dcad626f9c3e8d6
Kalei Alma <kalei@tuta.io>
2025-10-16 11:49:12 -0700

    ok this seems promising (linux arm)

--- cmake/toolchain/Linux-arm64.cmake
+++ cmake/toolchain/Linux-arm64.cmake
@@ -16,6 +16,8 @@ set(CMAKE_C_COMPILER_TARGET aarch64-linux-gnu)
 set(CMAKE_CXX_COMPILER_TARGET aarch64-linux-gnu)
 
 if(GITHUB_ACTIONS)
+    set(CMAKE_SYSROOT /var/chroot/noble-arm64)
+
     set(CMAKE_C_STANDARD_LINK_DIRECTORIES /usr/lib/aarch64-linux-gnu)
     set(CMAKE_CXX_STANDARD_LINK_DIRECTORIES /usr/lib/aarch64-linux-gnu)
 
