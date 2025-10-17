b96f1bb3a5c6b47c0713a3ebb0f919c5ee4a5c70
Kalei Alma <kalei@tuta.io>
2025-10-17 14:15:09 -0700

    fuck stb_vorbis

--- external/headerlibs.cpp
+++ external/headerlibs.cpp
@@ -4,6 +4,7 @@
 #define MA_IMPLEMENTATION
 #define MA_USE_STDINT
 #include "miniaudio.h"
+#undef L // fuck stb_vorbis
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
